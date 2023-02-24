d37f1dc7374c11fdde8829b2e6228a5f70057fe5
EcemAydn <ecemaydn15@gmail.com>
2023-02-24 16:18:06 +0300

    promise

--- src/stores/login.js
+++ src/stores/login.js
@@ -11,40 +11,30 @@ export const useLoginStore = defineStore('login', () => {
   
 
   function addUser(user){
-    axios.post('https://taskmanager.ron.digital/api/login',{
-      ...user
-    }).then(response => {
-      console.log(response);
-    })
-    .catch (error => {
-      console.log(error);
-    })
-  }
-  return {user, addUser}
+    let deneme= false;
+    new Promise(function(resolve,reject){
+      const timeout= setTimeout(() => {
+        if(deneme) {
+          resolve('okeyto');
+        }
+      },1000)
+      axios.post('https://taskmanager.ron.digital/api/login',{
+        ...user
+      }).then(response => {
+        console.log(response);
+        clearTimeout(timeout);
+        if(!deneme) {
+          console.log('fetch good! ', response);
+          resolve(alert('basarili'));
+        }
+      })
+      .catch (error => {
+        console.log(error);
+        reject(alert('olmadi'))
+      })
+    });
+    }
+    return {user, addUser}
  
 })
 
-// import { defineStore } from 'pinia'
-// import axios from "axios"
-// export const useUserStore = defineStore("user", {
-//     state: () => ({
-//         users: [],
-//     }),
-//     getters: {
-//       getUsers(state){
-//           return state.users
-//         }
-//     },
-//     actions: {
-//       async fetchUsers() {
-//         try {
-//           const data = await axios.post('https://taskmanager.ron.digital/api/login', {user})
-//             this.users = data.data
-//           }
-//           catch (error) {
-//             alert(error)
-//             console.log(error)
-//         }
-//       }
-//     },
-// })
