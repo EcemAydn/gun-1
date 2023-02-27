7052fbd67b8969b41ae1fa68b6d3e5de4d028b6f
EcemAydn <ecemaydn15@gmail.com>
2023-02-27 08:45:40 +0300

    promise ve api islemleri

--- src/stores/login.js
+++ src/stores/login.js
@@ -7,33 +7,63 @@ export const useLoginStore = defineStore('login', () => {
     email:'',
     password:''
   });
+  const titles = ref([])
+  const newInstance = axios.create({
+    baseURL: 'https://taskmanager.ron.digital/api'
+  })
 
-  
+  function bla(){
+    return new Promise((resolve, reject) => {
+
+      newInstance.get('/titles', {
+        }).then(response => {
+          console.log(response.data.titles);
+          titles.value = response.data.titles;
+          resolve()
+        }).catch(()=> {
+          reject('olmadi')
+        })
+    })
+  }
+  function ups(uv){ 
+    return new Promise((resolve, reject) => {
+    newInstance.put(`/titles/${uv.id}`, {
+      name: uv.name, description: uv.description
+     }).then(response => {
+       console.log(response.data.titles);
+       resolve('oldu');
+     }).catch(()=>{
+      reject('olmadi');
+     })
+    })
+  }
+ 
+  function getUser() {
+    if(localStorage.getItem('token')){
+      newInstance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+    }
+  }
 
   function addUser(user){
-    let deneme= false;
-    new Promise(function(resolve,reject){
-      const timeout= setTimeout(() => {
-        if(deneme) {
-          resolve('okeyto');
-        }
-      },1000)
+     return new Promise((resolve, reject) => {
       axios.post('https://taskmanager.ron.digital/api/login',{
-        ...user
+       email:user.email, password: user.password
       }).then(response => {
-        console.log(response);
-        clearTimeout(timeout);
-        if(!deneme) {
-          resolve(alert('basarili'));
-        }
+        localStorage.setItem('token',response.data.token);
+        newInstance.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`
+        resolve('oldu')
+      }).catch(()=> {
+        reject('olmadi')
       })
-      .catch (error => {
-        console.log(error);
-        reject(alert('olmadi'))
-      })
-    });
+     }) 
     }
-    return {user, addUser}
+
+  // function deleteUser(id){
+  //   const findIndex = userList.value.findIndex((user)=> user.id === id);
+  //   userList.value.splice(findIndex, 1);
+  // }
+    
+  return {addUser, getUser, bla }
  
 })
 
