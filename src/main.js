b0b7b663b3e0506b1b34d5d3106ce9ad20a6bb08
EcemAydn <ecemaydn15@gmail.com>
2023-02-27 08:44:01 +0300

    router oluşturuldu

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-// import router from './router'
+import router from './router'
 
 import './assets/main.css'
 
@@ -10,6 +10,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 
-// app.use(router)
+app.use(router)
 
 app.mount('#app')
