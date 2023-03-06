908f7f7a1d9e3aca14bf0c2b62b5e0871fa5c4e0
EcemAydn <ecemaydn15@gmail.com>
2023-03-06 08:54:28 +0300

    arkaplan rengi değişti

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ userStore.getUser();
 </script>
 
 <template>
-  <div class="w-full h-full flex flex-col items-center justify-center text-sm gap-6 bg-purple-100">
+  <div class="w-full h-full flex flex-col items-center justify-center text-sm gap-6 bg-gray-100">
     <RouterView />
   </div>
 </template>
