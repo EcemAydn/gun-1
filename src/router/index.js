193df465e2e98927d7d6ea874789019db7007fa0
EcemAydn <ecemaydn15@gmail.com>
2023-03-06 08:48:17 +0300

    router duzenlendi

--- src/router/index.js
+++ src/router/index.js
@@ -11,43 +11,73 @@ const router = createRouter({
     {
       path: '/titles',
       name: 'title',
-      component: () => import('../views/TitleView.vue')
+      component: () => import('../views/Titles/TitleView.vue')
     },
     {
         path: '/update/:id',
         name: 'update',
-        component: () => import('../views/UpdateView.vue')
+        component: () => import('../views/Titles/UpdateView.vue')
     },
     {
         path: '/title/create',
         name: 'createTitle',
-        component: () => import('../views/CreateTitle.vue')
+        component: () => import('../views/Titles/CreateTitle.vue')
     },
     {
       path: '/members',
       name: 'members',
-      component: () => import('../views/MembersView.vue')
+      component: () => import('../views/Members/MembersView.vue')
     },
     {
       path: '/members/create',
       name: 'createMember',
-      component: () => import('../views/CreateMember.vue')
+      component: () => import('../views/Members/CreateMember.vue')
     },
     {
       path: '/members/update/:id',
       name: 'UpdateMember',
-      component: () => import('../views/UpdateMember.vue')
+      component: () => import('../views/Members/UpdateMember.vue')
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: () => import('../views/Projects/ProjectView.vue')
+    },
+    {
+      path: '/projects/create',
+      name: 'CreateProject',
+      component: () => import('../views/Projects/CreateProject.vue')
+    },
+    {
+      path: '/projects/update/:id',
+      name: 'UpdateProject',
+      component: () => import('../views/Projects/UpdateProject.vue')
+    },
+    {
+      path: '/missions',
+      name: 'missions',
+      component: () => import('../views/Missions/MissionsView.vue')
+    },
+    {
+      path: '/missions/create',
+      name: 'CreateMission',
+      component: () => import('../views/Missions/CreateMissions.vue')
+    },
+    {
+      path: '/missions/update/:id',
+      name: 'UpdateMission',
+      component: () => import('../views/Missions/UpdateMissions.vue')
     },
   ]
 });
 
-const token = localStorage.getItem('token');
-router.beforeEach((to, from, next) => {
-   if(!token && to.name !== 'login') {
-    next({ name: 'login' });
-   }
+// const token = localStorage.getItem('token');
+// router.beforeEach((to, from, next) => {
+//    if(!token && to.name !== 'login') {
+//     next({ name: 'login' });
+//    }
 
-   next();
-})
+//    next();
+// })
 
 export default router
